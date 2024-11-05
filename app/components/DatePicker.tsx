5d11d753ea3bed18321b79292843788c61ec90d9
alexvcs <alexcurtisslep@gmail.com>
2024-11-05 13:49:37 -0500

    commented out datePicker

--- app/components/DatePicker.tsx
+++ app/components/DatePicker.tsx
@@ -1,19 +1,19 @@
-interface DatePickerProps {
-  setSelectedDate: (date: Date) => void;
-  datePickerValue: string;
-  setDatePickerValue: (value: string) => void;
-}
+// interface DatePickerProps {
+//   setSelectedDate: (date: Date) => void;
+//   datePickerValue: string;
+//   setDatePickerValue: (value: string) => void;
+// }
 
-function DatePicker({ setSelectedDate, datePickerValue, setDatePickerValue }: DatePickerProps) {
-  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const utcDate = new Date(event.target.value);
-    setSelectedDate(utcDate);
-    setDatePickerValue(event.target.value);
-  };
+// function DatePicker({ setSelectedDate, datePickerValue, setDatePickerValue }: DatePickerProps) {
+//   const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+//     const utcDate = new Date(event.target.value);
+//     setSelectedDate(utcDate);
+//     setDatePickerValue(event.target.value);
+//   };
 
-  return (
-    <input type="date" value={datePickerValue} onChange={handleDateChange} />
-  );
-}
+//   return (
+//     <input type="date" value={datePickerValue} onChange={handleDateChange} />
+//   );
+// }
 
-export default DatePicker;
+// export default DatePicker;
