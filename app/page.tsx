76889dfce2b3fa810d8d3eefc75649e47050db41
alexvcs <alexcurtisslep@gmail.com>
2024-12-13 12:51:04 -0500

    if selected days games haven't happened, hide show scores slider

--- app/page.tsx
+++ app/page.tsx
@@ -7,39 +7,34 @@ import {Switch} from "@/components/ui/switch";
 import DatePicker from "./components/DatePicker";
 
 function Home() {
-  // const [datePickerValue, setDatePickerValue] = useState("");
   const [showScores, setShowScores] = useState(false);
   const [dateSelected, setDateSelected] = useState<Date>();
 
-  const todaysDate = new Date().toDateString();
-  // console.log(todaysDate);
-
-  // console.log(dateSelected && format(dateSelected, 'yyyy-MM-dd'))
+  const todaysDate = new Date();
 
   return (
     <div className="text-center flex flex-col gap-2">
       <h1 className="mt-2">NBA Scores</h1>
-      <div>Today is {todaysDate}</div>
-      <div>
-        <Switch
-          className="mr-2 data-[state=unchecked]:bg-[#E47041] data-[state=checked]:bg-[#E47041]"
-          id="show-scores"
-          onCheckedChange={() => setShowScores(!showScores)}
-        />
-        <Label htmlFor="show-scores">
-          {showScores ? "Hide Scores" : "Show Scores"}
-        </Label>
-      </div>
+      <div>Today is {todaysDate.toDateString()}</div>
+      {dateSelected && dateSelected < todaysDate && (
+        <div>
+          <Switch
+            className="mr-2 data-[state=unchecked]:bg-[#E47041] data-[state=checked]:bg-[#E47041]"
+            id="show-scores"
+            onCheckedChange={() => setShowScores(!showScores)}
+          />
+          <Label htmlFor="show-scores">
+            {showScores ? "Hide Scores" : "Show Scores"}
+          </Label>
+        </div>
+      )}
       <div>
         <DatePicker
           dateSelected={dateSelected}
           setDateSelected={setDateSelected}
         />
       </div>
-      <Scores
-        showScores={showScores}
-        dateSelected={dateSelected}
-      />
+      <Scores showScores={showScores} dateSelected={dateSelected} />
     </div>
   );
 }
