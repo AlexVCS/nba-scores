a5f2652a36b2b00fb47ef44f43882e05a0b6a477
Alex Curtis-Slep <49458917+AlexVCS@users.noreply.github.com>
2026-01-23 10:17:15 -0500

    Boxscores (#134)
    * connect scores portion of the app to fastapi server, delete unused venv's
    
    * bring back boxscore stats for each player and their headshots
    
    * update gitignore
    
    * comment out inactive players component and parts of the game summary that need to be adjusted
    
    * comment out unused code for now
    
    * update Boxscore to comment out stuff atm

--- src/routes/games/boxscore/Boxscore.tsx
+++ src/routes/games/boxscore/Boxscore.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { useParams, useSearchParams } from "react-router";
-import GameSummary from "@/components/GameSummary";
+// import GameSummary from "@/components/GameSummary";
 import DarkModeToggle from "@/components/DarkModeToggle";
 import PlayerTable from "./PlayerTable";
-import InactivePlayers from "./InactivePlayers";
-import { getScores, getBoxScores } from "@/services/nbaService";
+// import InactivePlayers from "./InactivePlayers";
+import { getBoxScores } from "@/services/nbaService";
 
 const Boxscore = () => {
   const { gameId = "" } = useParams();
@@ -16,12 +16,12 @@ const Boxscore = () => {
     queryFn: () => getBoxScores(gameId),
   });
 
-  const gamesQuery = useQuery({
-    queryKey: ["games"],
-    queryFn: () => getScores(),
-  });
+  // const gamesQuery = useQuery({
+  //   queryKey: ["games"],
+  //   queryFn: () => getScores(),
+  // });
 
-  if (boxscoreQuery.isLoading || gamesQuery.isLoading) {
+  if (boxscoreQuery.isLoading) {
     return <h1>Loading...</h1>;
   }
 
@@ -29,9 +29,14 @@ const Boxscore = () => {
     return <h1>Error loading boxscore data</h1>;
   }
 
-  const specificGame = gamesQuery.data?.games.find(
-    (g: any) => g.gameId === gameId
-  );
+  // const specificGame = gamesQuery.data?.games.find(
+  //   (g: any) => g.gameId === gameId
+  // );
+
+  console.log("gameId from URL:", gameId);
+console.log("date from URL:", date);
+// console.log("gamesQuery.data:", gamesQuery.data);
+// console.log("specificGame:", specificGame);
 
   console.log("gameId from URL:", gameId);
 console.log("date from URL:", date);
@@ -43,10 +48,10 @@ console.log("specificGame:", specificGame);
   return (
     <div className="bg-slate-50 dark:bg-neutral-950">
       <DarkModeToggle />
-      <GameSummary game={specificGame} />
+      {/* <GameSummary game={specificGame} /> */}
       <PlayerTable team={game.homeTeam} />
       <PlayerTable team={game.awayTeam} />
-      <InactivePlayers game={game} />
+      {/* <InactivePlayers game={game} /> */}
     </div>
   );
 };
