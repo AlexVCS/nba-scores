ad81c592877a20571b865131391fd6d297685746
AlexVCS <alexcurtisslep@gmail.com>
2026-06-09 10:03:37 -0400

    add new domain to allowed origins in server

--- server/main.py
+++ server/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://nba-scorez.onrender.com", "http://localhost:5173"],
+    allow_origins=["https://nba-scorez.onrender.com", "https://nbascorez.com", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
